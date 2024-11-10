a4f68e3856005dd47e21808398ba64021ae5de8c
Gulcar <92885524+Gulcar@users.noreply.github.com>
2024-11-10 11:12:45 +0100

    add test_rock.obj

--- src/Main.cpp
+++ src/Main.cpp
@@ -14,7 +14,8 @@ int main()
 
     renderer::create_window("Kvejken", 1280, 720);
 
-    Model model("../../assets/test_cube.obj");
+    Model test_cube("../../assets/test_cube.obj");
+    Model test_rock("../../assets/test_rock.obj");
 
     while (renderer::is_window_open())
     {
@@ -22,13 +23,16 @@ int main()
 
         renderer::clear_screen();
 
-        renderer::draw_model(&model, glm::vec3(std::sin(glfwGetTime()), 0, 0), glm::vec3(1, 1, 1), glm::vec3(0, glfwGetTime(), 0));
-        renderer::draw_model(&model, glm::vec3(3, 0.4f, -5), glm::vec3(0.5f), glm::vec3(0, 0, 0));
-        renderer::draw_model(&model, glm::vec3(2, 0, -3), glm::vec3(0.5f), glm::vec3(0, 0, 0));
+        renderer::draw_model(&test_cube, glm::vec3(std::sin(glfwGetTime()), 0, 0), glm::vec3(1, 1, 1), glm::vec3(0, glfwGetTime(), 0));
+        renderer::draw_model(&test_cube, glm::vec3(3, 0.4f, -5), glm::vec3(0.5f), glm::vec3(0, 0, 0));
+        renderer::draw_model(&test_cube, glm::vec3(2, 0, -3), glm::vec3(0.5f), glm::vec3(0, 0, 0));
         for (int i = 0; i < 100; i++)
         {
-            renderer::draw_model(&model, glm::vec3(i, 0, -3), glm::vec3(0.5f), glm::vec3(0, 0, 0));
+            renderer::draw_model(&test_cube, glm::vec3(i, 0, -3), glm::vec3(0.5f), glm::vec3(0, 0, 0));
         }
+
+        renderer::draw_model(&test_rock, glm::vec3(4, 0, 0), glm::vec3(1, 1, 1), glm::vec3(0, -glfwGetTime(), 0));
+
         renderer::draw_queue();
 
         renderer::swap_buffers();
